d34454fe56f6dbc7a20d149e51d21e55e6964bb7
agates <agates@omnitechelectronics.ca>
2023-11-22 15:29:29 -0400

    cleanup and editable meal widgets

--- lib/main.dart
+++ lib/main.dart
@@ -13,33 +13,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'MealPlanner Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 183, 58, 102)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'MealPlanner Demo'),
+      home: const LandingPage(title: 'MealPlanner Demo'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
+  // This widget is the landing page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
 
@@ -51,53 +36,28 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LandingPage> createState() => _LandingPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // This method is rerun every time setState is called
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: 
        Container(
         // Outermost container with rounded edges and a black border
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16.0),
-          border: Border.all(color: Colors.black, width: 2.0), // Thicker outermost border
+          border: Border.all(color: Colors.black, width: 2.0),
         ),
-        margin: EdgeInsets.all(128.0), // More white space around the outermost container
-        padding: EdgeInsets.all(16.0), // Padding for inner content
+        margin: const EdgeInsets.all(128.0), // More white space around the outermost container
+        padding: const EdgeInsets.all(16.0), // Padding for inner content
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
@@ -110,24 +70,15 @@ class _MyHomePageState extends State<MyHomePage> {
                     borderRadius: BorderRadius.circular(8.0),
                     border: Border.all(color: Colors.grey[300]!),
                   ),
-                  padding: EdgeInsets.all(8.0), // Padding for inner content
-                  margin: EdgeInsets.symmetric(horizontal: 4.0),
+                  padding: const EdgeInsets.all(8.0), // Padding for inner content
+                  margin: const EdgeInsets.symmetric(horizontal: 4.0),
                   child: Column(
                     children: [
                       Text(day),
                       // Inner widgets representing meals
                       for (int j = 0; j < 3; j++)
                         Expanded(
-                          child: Container(
-                            // Container for each meal with rounded edges and a black border
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8.0),
-                              border: Border.all(color: Colors.black),
-                            ),
-                            margin: EdgeInsets.symmetric(vertical: 4.0), // Margin between meals
-                            alignment: Alignment.center,
-                            child: Text('Meal $j'), // Placeholder text for meals
-                          ),
+                          child: MealWidget(),
                         ),
                     ],
                   ),
@@ -136,38 +87,38 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
        ),
-        
-        /*Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),*/
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
+class MealWidget extends StatefulWidget {
+  const MealWidget({super.key});
+
+  @override
+  _MealWidgetState createState() => _MealWidgetState();
+}
+
+class _MealWidgetState extends State<MealWidget> {
+  final TextEditingController _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // Container for each meal with rounded edges and a black border
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+        border: Border.all(color: Colors.black),
+      ),
+      margin: const EdgeInsets.symmetric(vertical: 4.0), // Margin between meals
+      child: TextField(
+        controller: _textController,
+        decoration: const InputDecoration(
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.all(8.0),
+        ),
+      ),
     );
   }
 }
